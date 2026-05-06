a5a7bdf86e27c269c35c214c79e5801543ee7189
ylapiy <ygorgivagooo@gmail.com>
2026-05-05 23:16:26 -0300

    docs : adicionado um comentario

--- Teste_E2E/scrap.py
+++ Teste_E2E/scrap.py
@@ -5,6 +5,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from Paginas import *
 
+## o ideal seria passar a senha, o user a url e os dados pessoais para dentro de uma env
+## MAS como todos essas dados são publico / exemplos apenas para o trabalho, acho isso
+## desnecessario apenas faria que se alguem quisesse rodar isso localmente teria que setar a
+## env por si mesma o que so adiciona trabalho desnecesario
+
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
@@ -21,6 +26,7 @@ try:
     print("Itens adicionados e carrinho aberto.")
     paginaChekout(wait)
     paginaColocarDadosPessoais(wait)
+    print("Adicionado dados pesoais de exemplo")
     paginaFinalizandoACompra(wait)
     print("Compra finalizada com sucesso!")
 
